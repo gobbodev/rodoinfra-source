6d661532e7ef73033af2f74e4074ea32ad321cd8
Gabriel <gabrielgobbo.contato@protonmail.com>
2023-11-13 11:41:37 -0300

    feat: favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -32,6 +32,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="pt-BR">
         <Head>
+          
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
